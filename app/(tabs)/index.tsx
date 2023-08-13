f31cb628f313034c0d176cbc4b5e8907cd685d57
Rangga-PR <ranggaputraraharja@gmail.com>
2023-08-14 02:45:53 +0700

    add movie list / home screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,23 @@
-import { StyleSheet } from 'react-native';
+import { styles } from "../../styles";
+import { View } from "../../components/Themed";
+import { ActivityIndicator, FlatList } from "react-native";
+import { useGetMoviesQuery } from "../../store/api/movie";
+import MovieCard from "../../components/MovieCard";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+export default function HomeScreen() {
+  const { data, isLoading } = useGetMoviesQuery(1);
 
-export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={styles.outerContainer}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          keyExtractor={(item) => item.id.toString()}
+          data={data?.results}
+          renderItem={({ item }) => <MovieCard movie={item} action={["add"]} />}
+        />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
